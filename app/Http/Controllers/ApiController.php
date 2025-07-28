b34fd496b8d0234fd09ebfd0482dd8c713307492
JHenrique <henryque.pc.game@gmail.com>
2025-07-28 19:13:13 -0300

    Dados de Um Cliente

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -24,4 +24,32 @@ class ApiController extends Controller
             'data' => $clients
         ],200);
     }
+
+    public function clientById($id)
+    {
+        $client = Client::find($id);
+        return response()->json([
+            'status' => 'OK',
+            'message' => 'success',
+            'data' => $client
+        ],200);
+    }
+
+    public function client(Request $request)
+    {
+        // ckeck if id is provided
+        if ($request->id) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Client Id is invalid',
+            ],400);
+        }
+
+        $client = Client::find($request->id);
+        return response()->json([
+            'status' => 'OK',
+            'message' => 'success',
+            'data' => $client
+        ],200);
+    }
 }
