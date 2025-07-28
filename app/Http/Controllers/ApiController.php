2eccc0104addd18e871b4a0d8307879002d04bf0
JHenrique <henryque.pc.game@gmail.com>
2025-07-28 16:19:39 -0300

    Retornar JSON e Status

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
     public function status()
     {
-        return "Status OK";
+        return response()->json([
+            'status' => 'OK',
+            'message' => 'Api is running OK!',
+        ],200);
     }
 
     public function clients()
     {
-        return "All Clients";
+       $clients = Client::all();
+        return response()->json([
+            'status' => 'OK',
+            'message' => 'success',
+            'data' => $clients
+        ],200);
     }
 }
